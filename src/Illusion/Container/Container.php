e11089abc35e8c89484b2549d380b2f155ff0e51
oxy <renatofilipe.r7@gmail.com>
2015-02-16 13:17:49 +0000

    Added basic Container functionality.

---
+++ src/Illusion/Container/Container.php
@@ -0,0 +1,181 @@
+<?php
+
+namespace Illusion\Container;
+
+use Closure;
+use ArrayAccess;
+use ReflectionClass;
+
+class Container implements ArrayAccess
+{
+    private $bindings = [];
+
+    /**
+     * Instanciates a new Container.
+     * @param array $args
+     */
+    public function __construct($args = [])
+    {
+        $this->bindings = $args;
+    }
+
+    /**
+     * Registers a binding in the Container.
+     * @param  string $key
+     * @param  mixed  $value
+     * @return void
+     */
+    public function register($key, $value)
+    {
+        $this->offsetSet($key, $value);
+    }
+
+    /**
+     * Resolves a binding in the Container.
+     * @param  string $key
+     * @param  array  $args
+     * @return mixed
+     */
+    public function resolve($key, $args = [])
+    {
+        $class = $this->get($key);
+
+        if ($class === null) {
+            $class = $key;
+        }
+
+        if (is_string($class)) {
+            return $this->direct($class);
+        }
+
+        if ($class instanceof Closure) {
+            return $this->closure($class);
+        }
+    }
+
+    /**
+     * Resolves an instance based on the class name.
+     *
+     * @param string $class
+     * @param array  $args
+     * @return mixed
+     */
+    protected function direct($class)
+    {
+        $args = [];
+
+        $reflect = new ReflectionClass($class);
+
+        if ( ! $reflect->isInstantiable()) {
+            throw new Exception(sprintf('"%s" is not instantiable.'));
+        }
+
+        if ( ! is_null($reflect->getConstructor())) {
+            $dependencies = $reflect->getConstructor()->getParameters();
+
+            foreach ($dependencies as $dependency) {
+                if ($dependency->isArray() || $dependency->isOptional()) continue;
+
+                $class = $dependency->getClass();
+
+                if (is_null($class)) continue;
+
+                array_unshift($args, $this->resolve($class->name));
+            }
+        }
+
+        return $reflect->newInstanceArgs($args);
+    }
+
+    /**
+     * Resolves an instance based on a passed closure.
+     *
+     * @param object $class
+     * @param array  $args
+     * @return mixed
+     */
+    protected function closure($callback, $args = [])
+    {
+        return call_user_func_array($callback, array());
+    }
+
+    /**
+     * Gets a given binding value.
+     * @param  string $key
+     * @return mixed
+     */
+    public function get($key)
+    {
+        return $this->offsetGet($key);
+    }
+
+    /**
+     * Checks if the Container has a given binding.
+     * @param  string $key
+     * @return boolean
+     */
+    public function has($key)
+    {
+        return isset($this->bindings[$key]);
+    }
+
+    /**
+     * Removes an given binding from the Container it it exists.
+     * @param  string $key
+     * @return void
+     */
+    public function delete($key)
+    {
+        $this->offsetUnset($key);
+    }
+
+    /**
+     * Gets a given binding value.
+     * @param  string $key
+     * @return mixed
+     */
+    public function offsetGet($key)
+    {
+        if ( ! $this->has($key)) {
+            return null;
+        }
+
+        return $this->bindings[$key];
+    }
+
+    /**
+     * Registers a binding in the Container.
+     * @param  string $key
+     * @param  mixed  $value
+     * @return void
+     */
+    public function offsetSet($key, $value)
+    {
+        if (is_null($key)) {
+            $this->bindings[] = $value;
+        } else {
+            $this->bindings[$key] = $value;
+        }
+    }
+
+    /**
+     * Checks if the Container has a given binding.
+     * @param  string $key
+     * @return boolean
+     */
+    public function offsetExists($key)
+    {
+        return isset($this->bindings[$key]);
+    }
+
+    /**
+     * Removes an given binding from the Container it it exists.
+     * @param  string $key
+     * @return void
+     */
+    public function offsetUnset($key) {
+        if ($this->has($key)) {
+            unset($this->bindings[$key]);
+        }
+    }
+}
---
+++ tests/Container/ContainerTest.php
@@ -0,0 +1,79 @@
+<?php
+
+class Foo {}
+
+class Bar {
+    public function __construct(Foo $foo) {}
+}
+
+use Illusion\Container\Container;
+
+class ContainerTest extends PHPUnit_Framework_TestCase
+{
+    private $c;
+
+    public function setUp()
+    {
+        $this->c = new Container;
+    }
+
+    public function tearDown()
+    {
+        $this->c = new Container;
+    }
+
+    public function testOffsets()
+    {
+        $this->c['foo'] = 'bar';
+        $this->assertEquals('bar', $this->c['foo']);
+        $this->assertTrue($this->c->has('foo'));
+        unset($this->c['foo']);
+        $this->assertFalse($this->c->has('foo'));
+    }
+
+    public function testIfBindingExists()
+    {
+        $this->c->register('foo', 'Bar');
+        $this->assertTrue($this->c->has('foo'));
+        $this->assertEquals('Bar', $this->c->get('foo'));
+    }
+
+    public function testBindingValue()
+    {
+        $this->c->register('foo', 'Bar');
+        $this->assertEquals('Bar', $this->c->get('foo'));
+    }
+
+    public function testReturningNullWhenBindingDoesnExist()
+    {
+        $this->assertNull($this->c->get('qux'));
+    }
+
+    public function testDirectResolve()
+    {
+        $this->c->register('foo', 'Foo');
+        $this->c->resolve('foo');
+        $this->assertInstanceOf('Foo', $this->c->resolve('foo'));
+
+        $this->c->register('bar', 'Bar');
+        $this->c->resolve('bar');
+        $this->assertInstanceOf('Bar', $this->c->resolve('bar'));
+    }
+
+    public function testClosureResolve()
+    {
+        $this->c->register('foo', function() {
+            return new Foo;
+        });
+
+        $this->assertInstanceOf('Foo', $this->c->resolve('foo'));
+
+        $this->c->register('bar', function() {
+            $foo = new Foo;
+            return new Bar($foo);
+        });
+
+        $this->assertInstanceOf('Bar', $this->c->resolve('bar'));
+    }
+
+}
